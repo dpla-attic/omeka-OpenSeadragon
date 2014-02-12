39a28bd6a0158ec447266e6bf3122a91f55ea5b8
Mark A. Matienzo <mark@matienzo.org>
2014-02-12 18:44:41 -0500

    Simplify the viewer construction and dynamically build tile source

--- views/shared/common/openseadragon.php
+++ views/shared/common/openseadragon.php
@@ -1,47 +1,35 @@
 <?php
-$unique_id = "openseadragon_".hash("md4", $images[0]->getWebPath('original'));
-$btn_path = src('images/', 'openseadragon');
+$button_path = src('images/', 'openseadragon');
 //$cache_param = html_escape(Zend_Registry::get('bootstrap')->getResource('Config')->dpla->openseadragon->update_cache_param);
 ?>
 
 <div class="openseadragon">
+    <?php
+    foreach($images as $image):
+        $image_url = $image->getWebPath('original');
+        $unique_id = "openseadragon_".hash("md4", $image_url);
+        $metadata = json_decode($image->metadata, true);
+        $image_url_esc = html_escape($image_url);
+       ?>
     <div class="openseadragon_viewer" id="<?=$unique_id?>"
-        data-api-url="<?=$btn_path?>home_pressed.png"> <!-- FIXME -->
-        <?php foreach($images as $image): 
-           $metadata = json_decode($image->metadata, true)?>
-            <img src="<?php echo html_escape($image->getWebPath('original')); ?>"
-            data-original="<?php echo html_escape($image->getWebPath('original')); ?>"
-            class="openseadragon-image tmp-img" alt="">
-        <?php endforeach; ?>
+        data-api-url="<?=$button_path?>home_pressed.png"> <!-- FIXME -->
+        <img src="<?=$image_url_esc?>" data-original="<?=$image_url_esc?>"
+        class="openseadragon-image tmp-img" alt="">
+    </div>
+
     <script type="text/javascript">
-        var viewer = OpenSeadragon({
-        id:              "<?=$unique_id?>",
-        prefixUrl:       "<?=$btn_path?>",
-        tileSources: {
-            type: 'legacy-image-pyramid',
-            // TODO: Add logic that checks the file type of original; if it's a TIFF,
-            //       fail gracefully by not including the original in the image pyramid
-            levels:[{
-                url: "<?=$images[0]->getWebPath('original')?>",
-                height: <?= $metadata['video']['resolution_y'] ?>,
-                width: <?= $metadata['video']['resolution_x'] ?>
-            }, {
-                url: "<?=$images[0]->getWebPath('fullsize')?>",
-                height: <?php echo get_option('fullsize_constraint'); ?>,
-                width: <?php echo get_option('fullsize_constraint'); ?>
-            }, {
-                url: "<?=$images[0]->getWebPath('thumbnail')?>",
-                height: <?php echo get_option('thumbnail_constraint'); ?>,
-                width: <?php echo get_option('thumbnail_constraint'); ?>
-            }
-            ]
-        },
-        showNavigationControl:   true, 
-        navigationControlAnchor: OpenSeadragon.ControlAnchor.BOTTOM_RIGHT,
-        minZoomImageRatio: 0.8,
-        maxZoomPixelRatio: 2,
-        autoHideControls: false
-    });
-    // </script>
-</div>
+        osdSettings(); // defined in shared/openseadragon/settings.js
+        // the following needs to be set dynamically
+        OpenSeadragon.DEFAULT_SETTINGS.prefixUrl = '<?=$button_path?>';
+        var viewer = OpenSeadragon("<?=$unique_id?>");
+
+        // TODO: Add logic that checks the file type of original; if it's a
+        // TIFF,  fail gracefully by not including the original in the image
+        // pyramid or generate a new JPEG
+        var ts = new OpenSeadragon.LegacyTileSource(
+            <?php echo openseadragon_create_pyramid($image); ?>
+        );
+        viewer.openTileSource(ts);
+    </script>
+    <?php endforeach; ?>
 </div>
